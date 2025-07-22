9ed325ad45beefab44b0c3f5e6f6e3508253fae1
Racheli-shtrochlitz <r0556735906@gmail.com>
2025-07-22 23:13:16 +0300

    Time: 11 ms (93.7%), Space: 93.1 MB (98.9%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int n=nums.size();
-        int left=0,
-        right=0,
-        sum=0,
-        maxVal=0;
-        set<int> set;
+        int n = nums.size();
+        int left = 0, right = 0;
+        int sum = 0, maxVal = 0;
+        vector<bool> seen(10001, false); // נניח המספרים עד 10^4
 
-        while(right<n){
-            if(!set.count(nums[right])){
-                sum+=nums[right];
-                maxVal=max(maxVal,sum);
-                set.insert(nums[right]);
+        while (right < n) {
+            if (!seen[nums[right]]) {
+                sum += nums[right];
+                maxVal = max(maxVal, sum);
+                seen[nums[right]] = true;
                 right++;
-            }
-            else{
-                sum-=nums[left];
-                set.erase(nums[left]);
+            } else {
+                sum -= nums[left];
+                seen[nums[left]] = false;
                 left++;
             }
         }
+
         return maxVal;
     }
 };
