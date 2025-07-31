6c8a470f0a3c704c8c0baa7b0cc83fd80b55525e
Racheli-shtrochlitz <r0556735906@gmail.com>
2025-07-31 21:12:28 +0300

    Time: 2 ms (9.96%), Space: 16.5 MB (52.38%) - LeetHub

--- 0828-chalkboard-xor-game/0828-chalkboard-xor-game.cpp
+++ 0828-chalkboard-xor-game/0828-chalkboard-xor-game.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool xorGame(vector<int>& nums) {
-    int x=0;
-    for(int n:nums) x^=n;
-    unordered_set<int> s(nums.begin(), nums.end());
-    return x==0 || nums.size() % 2 == (s.size()*2) % 2;
+    int x = 0;
+    for (int n : nums) x ^= n;
+    return x == 0 || nums.size() % 2 == 0;
     }
 };
