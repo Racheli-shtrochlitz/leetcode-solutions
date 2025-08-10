7735f56f84d084c683a9078e636c53bfe7bfddda
Racheli-shtrochlitz <r0556735906@gmail.com>
2025-08-10 16:49:51 +0300

    Time: 2 ms (34.81%), Space: 9.3 MB (27.14%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+    auto sortedDigits = [](int x) {
+        string s = to_string(x);
+        sort(s.begin(), s.end());
+        return s;
+    };
+
+    string target = sortedDigits(n);
+
+    for (int i = 0; i < 31; i++) { 
+        if (sortedDigits(1 << i) == target) {
+            return true;
+        }
+    }
+    return false;
+} 
+};
