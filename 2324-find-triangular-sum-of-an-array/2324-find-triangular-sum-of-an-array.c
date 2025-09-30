bae4663ffb2a2a95e41dcbefe2d8d159a899a5c3
Racheli-shtrochlitz <r0556735906@gmail.com>
2025-09-30 21:55:21 +0300

    Time: 51 ms (78.79%), Space: 8.9 MB (65.15%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.c
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.c
@@ -1,14 +1,8 @@
 int triangularSum(int* nums, int numsSize) {
-    if(numsSize==1) return nums[0];
-    int *newNums = malloc((numsSize-1 )* sizeof(int));
-     if (newNums == NULL) {
-        printf("שגיאה בהקצאה\n");
-        exit(1); 
+    for (int step = numsSize; step > 1; step--) {
+        for (int i = 0; i < step - 1; i++) {
+            nums[i] = (nums[i] + nums[i + 1]) % 10;
+        }
     }
-    for(int i=0; i<numsSize-1; i++){
-        newNums[i]=(nums[i]+nums[i+1])% 10;
-    }
-    int result = triangularSum(newNums, numsSize - 1);
-    free(newNums); 
-    return result;
+    return nums[0];
 }
