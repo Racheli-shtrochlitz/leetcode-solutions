6d375c50acd3f854414051ae86f56aaa9881c16f
Racheli-shtrochlitz <r0556735906@gmail.com>
2025-10-29 21:59:49 +0200

    Time: 0 ms (100%), Space: 8.4 MB (30.05%) - LeetHub

---
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int bits=(int)floor(log2(n))+1; //מספר הסיביות במספר
+        if((n+1) & n == 0) return n;
+        return (1u <<bits) -1;
+    }
+};
